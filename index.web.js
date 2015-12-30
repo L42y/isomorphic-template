2ecf82db3fc2f53e7ff26d467d65f0394541eb71
L42y <423300@gmail.com>
2015-12-30 17:50:59 +0800

    es6

--- index.web.js
+++ index.web.js
@@ -1,15 +1,15 @@
 'use strict';
 
 import React from 'react';
-import ReactDOM from 'react-dom';
-import Router from 'react-router';
+import {render} from 'react-dom';
+import {Router} from 'react-router';
 import createBrowserHistory from 'history/lib/createBrowserHistory';
 
 import routes from './routes.js';
 
-let history = createBrowserHistory();
+const history = createBrowserHistory();
 
-ReactDOM.render(
+render(
   <Router history={history}>{routes}</Router>,
   document.getElementById('react-root')
 );
