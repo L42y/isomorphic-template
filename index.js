719e8f41b53af2967f933d4f4ded323adf20e59f
L42y <423300@gmail.com>
2016-12-27 12:49:56 +0800

    add minimum scale

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ const generateTemplate = ({title, markup}) => {
         <meta name="description"
               content=""/>
         <meta name="viewport"
-              content="width=device-width, initial-scale=1"/>
+              content="width=device-width, minimum-scale=1, initial-scale=1"/>
         <link rel="stylesheet"
               href="/!/web.bundle.css"/>
       </head>
